eeae07d071311fd2ac7db4aea5a05f06da611474
minami <south614fruit@yahoo.co.jp>
2025-10-23 22:29:51 +0900

    init

---
+++ src/app/Book.kt
@@ -0,0 +1,3 @@
+package app
+
+data class Book(var title: String, var publisher: String, var date: String, var authors: Array<String>)
---
+++ src/app/main.kt
@@ -0,0 +1,6 @@
+package app
+
+fun main(args : Array<String>) {
+    val book1 = Book("title", "publisher", "date", arrayOf("author"))
+    println(book1.title)
+}
